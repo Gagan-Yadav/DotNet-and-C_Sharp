447656298c46418a64873e842025b8b483650f63
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-02-15 19:37:52 +0530

    new commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Console.WriteLine("What is your name ?");
+// var name = Console.ReadLine();
+// var currentdate =  DateTime.Now;
+// Console.WriteLine("What is your name ?");
+// var age = Console.ReadLine();
+// Console.WriteLine($"{Environment.NewLine} Hello, {name} {age}, on {currentdate}");
+// Console.ReadKey(true);
+
+using System.Linq.Expressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Size of Array - ");
+        var size = int.TryParse(Console.ReadLine(), out var n);
+        int[] arr =new int[n];
+        Console.WriteLine("Enter Array elemenets - ");
+        for(int i =0;i<n;i++){
+            arr[i] = Convert.ToInt16(Console.ReadLine());
+        }
+        Console.Write("Enter Element to be search in array - ");
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int i =0;i<n;i++){
+            if(arr[i]==t){
+           Console.Write(arr[i]+" "+"gagan");
+            }
+        }
+       
+       
+        Console.ReadLine();
+    }
+}
---
+++ FrequencyOfCharacter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FrequencyOfCharacters{
+    static void Main(){
+        Console.WriteLine("Frequency  of Character!");
+        Dictionary<char, int> frequency = new Dictionary<char, int>();
+        Console.Write("Enter String : ");
+        string? input = Console.ReadLine();
+        foreach(char c in input){
+            if(char.IsWhiteSpace(c))
+            continue;
+
+            if(frequency.ContainsKey(c)){
+                frequency[c]++;
+            }else{
+                frequency[c] = 1;
+            }
+
+        }
+
+        Console.WriteLine("Frequency of Characters - ");
+        foreach(var item in frequency){
+            Console.Write($"{item.Key}:{item.Value}"+" ");
+        }
+
+    }
+}
