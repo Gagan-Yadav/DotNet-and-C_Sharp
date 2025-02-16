4e0c73641619111da77bd35d36a6c3d839563055
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-02-16 16:30:12 +0530

    Update

--- SuperiorElement/Program.cs
+++ SuperiorElement/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-class SuperiorElement{
-    static void Main(String[] args){
-        Console.WriteLine("Enter size of an array - ");
-        int size = Convert.ToInt16(Console.Read());
-        int[] arr = new int[size];
-        Console.WriteLine("Enter Array Elements - ");
-        for(int i=0;i<arr.Length;i++){
-            arr[i] = Convert.ToInt16(Console.Read());
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of elements in the array: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter the elements of the array:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
         }
+
+        List<int> superiorElements = FindSuperiorElements(arr);
+        
+        Console.WriteLine("Superior elements in the array:");
+        foreach (var elem in superiorElements)
+        {
+            Console.Write(elem + " ");
+        }
+    }
+
+    static List<int> FindSuperiorElements(int[] arr)
+    {
+        List<int> superiorElements = new List<int>();
+        int n = arr.Length;
+        int maxRight = int.MinValue;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (arr[i] > maxRight)
+            {
+                superiorElements.Add(arr[i]);
+                maxRight = arr[i];
+            }
+        }
+
+        superiorElements.Reverse(); // To maintain the order of appearance in input
+        return superiorElements;
     }
 }
