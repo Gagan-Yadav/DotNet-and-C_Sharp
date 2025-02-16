205290489c943326c6d670f1d5e63a515ab22c4e
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-02-16 16:18:30 +0530

    new commit

---
+++ SuperiorElement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class SuperiorElement{
+    static void Main(String[] args){
+        Console.WriteLine("Enter size of an array - ");
+        int size = Convert.ToInt16(Console.Read());
+        int[] arr = new int[size];
+        Console.WriteLine("Enter Array Elements - ");
+        for(int i=0;i<arr.Length;i++){
+            arr[i] = Convert.ToInt16(Console.Read());
+        }
+    }
+}
