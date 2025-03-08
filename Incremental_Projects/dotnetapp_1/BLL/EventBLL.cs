8a6e94fef0817231c3c513a94759affa8c279127
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-03-08 20:31:44 +0530

    Create EventBLL.cs

---
+++ Incremental_Projects/dotnetapp_1/BLL/EventBLL.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+// using Internal;
+namespace dotnetapp.BLL
+{
+    class EventBLL
+    {
+
+        public void ListEvents()
+        {
+            // if(events.Count>0){
+            //         foreach(Event item in events)
+            // }else{
+            //     Console.WriteLine("No Events Available!");
+            // }
+        }
+        public void AddEvent()
+        {
+
+            // events.Add();                
+        }
+        public void AddAttendeeToDB()
+        {
+
+        }
+        public void ListAttendeesFromDB()
+        {
+
+        }
+
+        public void ListEventsFromDB()
+        {
+
+        }
+        public void AddEventToDB()
+        {
+
+        }
+    }
+
+}
