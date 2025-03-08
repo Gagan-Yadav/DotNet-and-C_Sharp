e50f0c03ac30935a0994ef868c684b91bf559a7f
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-03-08 20:30:01 +0530

    Create IEventManager.cs

---
+++ Incremental_Projects/dotnetapp_1/Managers/IEventManager.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace dotnetapp.Managers
+{
+    interface IEventManager
+    {
+        public void AddEvent()
+        {
+
+        }
+        public void ListEvents()
+        {
+
+        }
+        public void AddEventToDB()
+        {
+
+        }
+        public void ListEventsFromDB()
+        {
+
+        }
+    }
+
+}
