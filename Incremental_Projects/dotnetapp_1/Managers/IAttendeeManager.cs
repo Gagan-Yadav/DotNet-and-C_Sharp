46266a6a99cf002d6fc73be790b6d3c5246d125e
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-03-08 20:30:42 +0530

    Create IAttendeeManager.cs

---
+++ Incremental_Projects/dotnetapp_1/Managers/IAttendeeManager.cs
@@ -0,0 +1,55 @@
+using System;
+namespace dotnetapp.Managers
+{
+
+    interface IAttendeeManager
+    {
+        public void AddAttendee(int EventId)
+        {
+
+        }
+
+        public void EditAttendee()
+        {
+
+        }
+
+        public void DeleteAttendee()
+        {
+
+        }
+
+        public void ListAttendees()
+        {
+
+        }
+
+        public void FindAttendee(string attendeeName)
+        {
+
+        }
+
+        public void AddAttendeeToDB(int eventid)
+        {
+
+        }
+
+        public void EditAttendeeInDB()
+        {
+
+        }
+
+        public void DeleteAttendeeFromDB()
+        {
+
+        }
+        public void ListAttendeesFromDB()
+        {
+
+        }
+
+
+
+    }
+
+}
