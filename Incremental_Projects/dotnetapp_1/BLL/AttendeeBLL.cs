25252401887715af624dc2b3f0a06d1887f52c29
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-03-08 20:32:32 +0530

    Create AttendeeBLL.cs

---
+++ Incremental_Projects/dotnetapp_1/BLL/AttendeeBLL.cs
@@ -0,0 +1,44 @@
+using System;
+namespace dotnetapp.BLL
+{
+    class AttendeeBLL
+    {
+        public void AddAttendee()
+        {
+
+        }
+        public void ListAttendees()
+        {
+
+        }
+        public void FindAttendee()
+        {
+
+        }
+        public void EditAttendee()
+        {
+
+        }
+
+        public void DeleteAttendee()
+        {
+
+        }
+        public void AddAttendeeToDB()
+        {
+
+        }
+        public void ListAttendeesFromDB()
+        {
+
+        }
+        public void EditAttendeeInDB()
+        {
+
+        }
+        public void DeleteAttendeeFromDB()
+        {
+
+        }
+    }
+}
