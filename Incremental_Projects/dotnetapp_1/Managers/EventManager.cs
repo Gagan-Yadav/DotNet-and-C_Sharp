7fb3829b38a8a39d39431cf0bce041a81ad33402
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-03-10 20:37:56 +0530

    Update EventManager.cs

--- Incremental_Projects/dotnetapp_1/Managers/EventManager.cs
+++ Incremental_Projects/dotnetapp_1/Managers/EventManager.cs
@@ -1,13 +1,15 @@
 using System;
 using dotnetapp.Models;
 // using Internal;
+// using Internal;
+// using Internal;
 namespace dotnetapp.Managers
 {
     class EventManager : IEventManager
     {
         // list here
-        public List<Event> events {get;set;} = new List<Event>();
-
+        public static List<Event> events {get;set;} = new List<Event>();
+        public static AttendeeManager am = new AttendeeManager();
         public void AddEvent()
         {
          
@@ -26,10 +28,29 @@ namespace dotnetapp.Managers
         }
         public void ListEvents()
         {
+            if(events.Count > 0){
             Console.WriteLine("List of Events:");
             foreach(Event item in events){
                Console.WriteLine($"Event ID: {item.EventId}, Name: {item.Name}, Location: {item.Location}, Event Date: {item.Date}, Event Budget: {item.Budget}");
+               am.ListAttendeeByID(item.EventId);
             }
+           
+
+            }else{
+                Console.WriteLine("No Recoord Found.");
+            }
+        }
+        public string ListEventByID(int id){
+            if(events.Count>0){
+                foreach(Event item in events){
+                    if(item.EventId==id){
+                        return item.Name;
+                    }
+                }
+            }
+
+                return null;
+        
         }
         public void AddEventToDB()
         {
