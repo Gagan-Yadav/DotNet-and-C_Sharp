c41779b80cb9fab6964f7aec5e3bfc1f6a419be7
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-03-14 11:17:02 +0530

    Create Program.cs

---
+++ ProjectTest Asp.Net/Program.cs
@@ -0,0 +1,31 @@
+using dotnetapp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("myconnstring"))); // write this line only
+// builder.Services.AddDbContext<AppDbContext1>(Options=> Options.UseSqlServer(builder.Configuration.GetConnectionString("con")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
