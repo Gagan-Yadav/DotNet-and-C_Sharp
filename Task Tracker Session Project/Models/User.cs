1ae59f89049aca5cdf6218eae0032412e9b7564b
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2025-03-15 13:29:40 +0530

    Create User.cs

---
+++ Task Tracker Session Project/Models/User.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnetapp.Models
+{
+    public class User
+    {
+
+        [Key]
+        public int Uid {get; set;}
+
+        public string UserName {get; set;}
+
+        public string Password {get; set;}
+        
+    }
+}
